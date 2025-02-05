815f39742533f41d3f85cfa947751fb4c822f31a
Cena Babajannjhad <cenababajannjhad@gmail.com>
2025-02-05 13:28:06 +0330

    d

--- src/main.js
+++ src/main.js
@@ -1,12 +1,12 @@
 // navbar handler
 import handlerMenu from "./script/util/hamburger-menu/hamburgerMenu.js";
 
-
 document.addEventListener("DOMContentLoaded" , () => {
     // navbar config
     const menuButton = document.querySelector("#menu__button");
     const closeMenuButton = document.querySelector("#close__button");
     menuButton.addEventListener("click", handlerMenu);
     closeMenuButton.addEventListener("click", handlerMenu);
+
 
 })
